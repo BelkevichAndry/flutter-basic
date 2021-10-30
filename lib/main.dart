6c9637f6fce2df29bbd44e7eb4c9d66d6a158539
BelkevichAndrew <abelkevich@remedypointsolutions.com>
2021-10-30 18:51:59 +0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -42,25 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Form(
-                key: _formKey,
-                child: TextFormField(
-                    controller: _controller,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(), labelText: "number"),
-                    validator: (value) {
-                      if (value!.isEmpty) return null;
-                      if (!RegExp(r"^\d+$").hasMatch(value)) {
-                        return 'We accept only numbers from 0-9';
-                      }
-                      return null;
-                    },
-                    onChanged: (context) => {
-                          if (_formKey.currentState!.validate())
-                            {_formKey.currentState!.save()}
-                        }))
-          ],
+          children: <Widget>[Text('Hello world text')],
         ),
       ),
     );
