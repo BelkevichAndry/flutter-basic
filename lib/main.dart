92a391b88a3ec2050b13757dddbc8f2b9abea97a
BelkevichAndrew <abelkevich@remedypointsolutions.com>
2021-10-30 18:50:39 +0300

    fix

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Form(
+                key: _formKey,
+                child: TextFormField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: "number"),
+                    validator: (value) {
+                      if (value!.isEmpty) return null;
+                      if (!RegExp(r"^\d+$").hasMatch(value)) {
+                        return 'We accept only numbers from 0-9';
+                      }
+                      return null;
+                    },
+                    onChanged: (context) => {
+                          if (_formKey.currentState!.validate())
+                            {_formKey.currentState!.save()}
+                        }))
+          ],
+        ),
+      ),
+    );
+  }
+}
